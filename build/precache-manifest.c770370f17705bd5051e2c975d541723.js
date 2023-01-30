792f5c679f29d7fb6b36adc753c2eb4e8302198c
Ighomena Daniel Odebala <71289078+dhee-tree@users.noreply.github.com>
2023-01-30 00:53:06 +0000

    Adding build

---
+++ build/precache-manifest.c770370f17705bd5051e2c975d541723.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9657912aaa376271944d7fd1e9e2b3b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576e3997c672fdee94a4",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "123f01cd30b23d2f05c5",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "e0bf7efa4634fc658064",
+    "url": "/static/css/main.44ce8deb.chunk.css"
+  },
+  {
+    "revision": "23bcd15312c86e84e809",
+    "url": "/static/js/2.a4718d94.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.a4718d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576e3997c672fdee94a4",
+    "url": "/static/js/3.98c7eac4.chunk.js"
+  },
+  {
+    "revision": "123f01cd30b23d2f05c5",
+    "url": "/static/js/4.b2569884.chunk.js"
+  },
+  {
+    "revision": "e0bf7efa4634fc658064",
+    "url": "/static/js/main.160dd7a4.chunk.js"
+  },
+  {
+    "revision": "1a45020977dcb6a0def9",
+    "url": "/static/js/runtime-main.e778a131.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "065f55e01625cd3f38a79d2af38d5ee1",
+    "url": "/static/media/edLogo.065f55e0.jpg"
+  },
+  {
+    "revision": "5640ef50169ad4cc3d90094e13e39842",
+    "url": "/static/media/elon.5640ef50.jpg"
+  },
+  {
+    "revision": "0e58e8d7710fd9dc484104a724cadd8d",
+    "url": "/static/media/favicon.0e58e8d7.png"
+  },
+  {
+    "revision": "4f167e6387a1a2b43b94524718894879",
+    "url": "/static/media/ighomena.4f167e63.jpg"
+  },
+  {
+    "revision": "c290a443b3f240732252a813d4a6cf65",
+    "url": "/static/media/python.c290a443.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "81875da9a99a17e12a2b0720022900c2",
+    "url": "/static/media/zeus.81875da9.jpg"
+  }
+]);
