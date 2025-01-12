7906e0f2a7585027bf8d650cd6e6d03c50457e66
Ighomena Daniel Odebala <ighoodes@gmail.com>
2025-01-12 00:03:51 +0000

    Update linkedin

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
 
 const socialMediaLinks = {
   github: "https://github.com/dhee-tree",
-  linkedin: "https://www.linkedin.com/in/ighomena-daniel-odebala/",
+  linkedin: "https://www.linkedin.com/in/ighomena/",
   twitter: 'https://twitter.com/ighomena__',
   instagram: 'https://www.instagram.com/dhee_tree/',
   // gmail: "ighoodes@gmail.com",
